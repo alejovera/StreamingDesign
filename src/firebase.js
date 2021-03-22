0270b4553456b928a3959096de70ec50d85e2023
Alejo <vera.alejo0903@gmail.com>
2021-03-22 12:14:31 -0300

    Cambios en archivos firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+const firebaseConfig = {
+    apiKey: "AIzaSyDaqZUBs9lp9fbyRqrYQ6QkWCaSasulxFY",
+    authDomain: "streaming-video2.firebaseapp.com",
+    projectId: "streaming-video2",
+    storageBucket: "streaming-video2.appspot.com",
+    messagingSenderId: "17447004008",
+    appId: "1:17447004008:web:0f96169a45519c922058dd",
+    measurementId: "G-B5JY9ME6NN"
+  };
+  const firebaseApp = firebase.initializeApp(firebaseConfig);
